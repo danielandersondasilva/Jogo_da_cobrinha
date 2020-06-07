0220cc5ee77629aab64af7c6d9341801ccd50711
Daniel Anderson <danielandersondasilva@gmail.com>
2020-06-07 20:59:33 -0300

    definir a fruta

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function update (event){
     if(event.keyCode == 38 && direction != 'down') direction = 'up';
     if(event.keyCode == 39 && direction != 'left') direction = 'right';
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
+
+    
 }
 
 function iniciarJogo(){
@@ -45,6 +47,8 @@ function iniciarJogo(){
     if(snake[0].x < 0 * box && direction == 'left') snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0 * box;
     if(snake[0].y < 0 * box && direction == 'up') snake[0].y = 16 * box;
+
+    
 
     criaBG();
     criarCobrinha();
@@ -57,7 +61,12 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
 
     let newHead = {
         x: snakeX,
